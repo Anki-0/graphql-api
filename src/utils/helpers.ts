729c02da91865a226e7728feeaf5e8c59e596b97
Ankit Singh <singhankit25apr@gmail.com>
2023-07-06 23:59:48 +0530

    Add helper function to generate random alphanumeric string of length N

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { getRandomValues } from 'crypto';
 
 export function getFilename(metaUrl: string) {
   const __filename = fileURLToPath(metaUrl);
@@ -11,4 +12,11 @@ export function getDirname(metaUrl: string) {
   const __dirname = path.dirname(getFilename(metaUrl));
 
   return __dirname;
+}
+
+export function randomString(size: number) {
+  const i2hex = (i: number) => ('0' + i.toString(16)).slice(-2);
+  const r = (a: string, i: number): string => a + i2hex(i);
+  const bytes = getRandomValues(new Uint8Array(size));
+  return Array.from(bytes).reduce(r, '');
 }
